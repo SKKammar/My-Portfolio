761b5d4e7fa8ac14691a56c78385423dce5d025a
Santosh Kammar <santoshkkammar16@gmail.com>
2026-07-30 16:54:45 +0530

    remove realtime Github view from home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import { Hero } from '@/components/sections/Hero';
 import { About } from '@/components/sections/About';
 import { Projects } from '@/components/sections/Projects';
 import { Skills } from '@/components/sections/Skills';
-import { GithubSection } from '@/components/sections/GitHub';
 import { Contact } from '@/components/sections/Contact';
 import { SpaceBackground } from '@/components/SpaceBackground';
 
@@ -28,8 +27,6 @@ export default function Page() {
               <Projects />
 
               <Skills />
-
-              <GithubSection />
 
               <Contact />
 
